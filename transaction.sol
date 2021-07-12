75985338bb44cca9c976ad2a24ec40416530aff4
Sabyasachi Mishra <32372252+Sabyasachi17@users.noreply.github.com>
2021-07-12 14:59:37 +0530

    Update transaction.sol

--- transaction.sol
+++ transaction.sol
@@ -2,75 +2,28 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Storage {
-        address payable public owner;
-        constructor()
-        {
-            owner = payable(msg.sender);
-        }
+import "./registry.sol";
+
+contract transaction is Storage
+{
+    event pradaJourney(uint256 indexed _serial_number,address indexed _from, address indexed _to);
+    function changeOwner(uint256 _serial_number, address payable _id, string memory _name) public
+    {
+        require(msg.sender== products[_serial_number].owner.id);
+        products[_serial_number].owner.name=_name;
+        products[_serial_number].owner.id= _id;
         
-        modifier onlyOwner{
-            require(msg.sender == owner);
-            _;
-        }
+        emit pradaJourney(_serial_number,msg.sender,_id);
+    }
+    function buyPrada(address payable sender,address receiver,uint256 _serial_number,string memory _name) public payable{
+        require(msg.sender == products[_serial_number].owner.id);
         
-        modifier onlyProductOwner{
-            require(msg.sender == owner);
-            _;
-        }
-        
-        struct Location{
-            uint256 longitude;
-            uint256 latitude;
-            string location_name;
-        }
-        
-        struct Owner {
-            address payable id;
-            string name;
-        }
-        
-        //product struct
-        struct Product{
-            string manufacture_location;
-            string model;
-            uint256 serial_number;
-            uint256 price;
-            Owner owner;
-            uint256 createTime;
-            
-        }
-        
-        struct Warehouse{
-            string location;
-            string name;
-        }
-        
-        struct Store{
-            string location;
-            string name;
-        }
-        
-        mapping(uint256 => Product) public products;
-        mapping(uint256 => Warehouse) public warehouses;
-        mapping(uint256 => Store) public stores;
-        
-        function addProducts(string memory _location, string memory _model, uint256 _serial_number, uint256 _price, string memory Owner_name) onlyOwner public
-        {
-            address payable tempOwner = payable(msg.sender);
-            Owner memory new_owner = Owner(tempOwner,Owner_name);
-            Product memory new_product= Product(_location,_model,_serial_number,_price,new_owner, block.timestamp);
-            products[_serial_number] = new_product;
-            
-        }
-        
-        function addWareHouse(uint256 _serial_number, string memory _location,string memory _name) onlyOwner public{
-            Warehouse memory new_wh= Warehouse(_location,_name);
-            warehouses[_serial_number] = new_wh;
-        }
-        
-        function addStore(uint256 _serial_number, string memory _location,string memory _name) onlyOwner public{
-            Store memory new_store= Store(_location,_name);
-            stores[_serial_number]= new_store;
-        }
+        sender.transfer(msg.value);
+        changeOwner(_serial_number,payable(receiver),_name);
+    }
+    
+    function whereIsMyPrada(uint256 _serial_number) public view returns(Product memory)
+    {
+        return products[_serial_number];
+    }
 }
